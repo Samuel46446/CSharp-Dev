501376d8ac126127c7fa7ee2af1c1192166b15b0
Samuel46446 <156199948+Samuel46446@users.noreply.github.com>
2024-12-01 01:59:14 +0100

    contact and tasks exercices

---
+++ Tasks.cs
@@ -0,0 +1,11 @@
+﻿using C_Dev;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        ExoContact contact = new ExoContact();
+        contact.launch();
+    }
+}
